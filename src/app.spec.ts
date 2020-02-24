e774bff6ff3bf3b155ff56aad8cbbfe225a5e058
odan <odan3240@gmail.com>
2020-02-24 23:56:32 +0900

    chore(app): remove unnecessary files

--- src/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { add } from "./app";
-
-describe("app", (): void => {
-  describe("add", (): void => {
-    it("should be correct", (): void => {
-      expect(add(32, 10)).toBe(42);
-    });
-  });
-});
