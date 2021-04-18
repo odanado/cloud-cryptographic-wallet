5bf47395335152a1ab16f4ac9248ce3667f477a1
odan <odan3240@gmail.com>
2021-04-18 11:45:40 +0900

    test(e2e): add send-eth test

--- test/config/index.ts
+++ test/config/index.ts
@@ -1,4 +1,4 @@
-export const rpcUrl =
-  "https://ropsten.infura.io/v3/bd35010d62134981a9e82dff4708728b";
+export const rpcUrl = process.env.RPC_URL;
 export const region = "us-east-1";
 export const keyId = "e9005048-475f-4767-9f2d-0d1fb0c89caf";
+export const privateKey = process.env.PRIVATE_KEY;
---
+++ test/e2e/send-eth.ts
@@ -0,0 +1,35 @@
+import Web3 from "web3";
+
+import { KmsProvider } from "../../src/";
+import { region, keyId, rpcUrl, privateKey } from "../config";
+
+beforeEach(async () => {
+  const web3 = new Web3(rpcUrl);
+  web3.eth.accounts.wallet.add(
+    web3.eth.accounts.privateKeyToAccount(privateKey)
+  );
+  const provider = new KmsProvider(rpcUrl, { region, keyIds: [keyId] });
+  const accounts = await provider.getAccounts();
+
+  await web3.eth.sendTransaction({
+    to: accounts[0],
+    value: web3.utils.toWei("1", "ether"),
+  });
+});
+
+it("web3.js", async () => {
+  const target = "0x75b130ed5e51b00cc7c5b91b1288c8d8e549f678";
+  const provider = new KmsProvider(rpcUrl, { region, keyIds: [keyId] });
+
+  const web3 = new Web3(provider);
+  const accounts = await web3.eth.getAccounts();
+
+  const value = web3.utils.toWei("0.5", "ether");
+  await web3.eth.sendTransaction({
+    from: accounts[0],
+    to: target,
+    value,
+  });
+
+  await expect(web3.eth.getBalance(target)).resolves.toBe(value);
+});
