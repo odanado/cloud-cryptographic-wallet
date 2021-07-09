b6ea888489d7d1f34690ebe606b6c0159e86ebda
odan <odan3240@gmail.com>
2021-07-09 22:09:40 +0900

    chore: remove prettier/@typescript-eslint in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
       "plugin:@typescript-eslint/eslint-recommended",
       "plugin:@typescript-eslint/recommended",
       "plugin:prettier/recommended",
-      "prettier/@typescript-eslint"
+      "prettier"
     ],
     plugins: [
       "@typescript-eslint"
