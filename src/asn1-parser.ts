a2b789d8f04d58b3095b40634347b7153518e35f
Moreal <dev.moreal@gmail.com>
2021-08-17 18:51:24 +0900

    fix: apply suggestions

--- src/asn1-parser.ts
+++ src/asn1-parser.ts
@@ -20,9 +20,7 @@ function pad(params: {
 }): Buffer {
   const { buffer, length, element } = params;
 
-  const padding = Buffer.from(
-    [...Array(length - buffer.length)].map((x) => element)
-  );
+  const padding = Buffer.alloc(length - buffer.length, element);
   return Buffer.concat([padding, buffer]);
 }
 
@@ -37,7 +35,7 @@ export function parsePublicKey(buf: Buffer) {
 /**
  * Parse signature from the given signature.
  * @param buf The buffer of the signature which following ASN.1 format.
- * @returns The set consists of, `r` and `s`. The `r` and `s` are parsed from the given signature and converted to 32 statically sized format from ASN.1 format. For example, if there is 33-bytes unsigned integer  formatted by ASN.1, it will be converted into 32-length bytes which dropped the first byte. (e.g. `00e26f7c547cf497959af070ec7c43ebdbb3e4341395912d6ccc950b43e886781b` → `00e26f7c547cf497959af070ec7c43ebdbb3e4341395912d6ccc950b43e886781b`). And, if there is 31-bytes integer formatted by ASN.1, it will be padded into 32-length bytes with `0` byte. (e.g. `74e3e4d71e7385ae71042b0f99f7fbbf66e7760dd513ed2fcea754e2a9131c` → `0074e3e4d71e7385ae71042b0f99f7fbbf66e7760dd513ed2fcea754e2a9131c`).
+ * @returns The set consists of, `r` and `s`. The `r` and `s` are parsed from the given signature and converted to 32 statically sized format from ASN.1 format. For example, if there is 33-bytes unsigned integer  formatted by ASN.1, it will be converted into 32-length bytes which dropped the first byte. (e.g. `00e26f7c547cf497959af070ec7c43ebdbb3e4341395912d6ccc950b43e886781b` → `e26f7c547cf497959af070ec7c43ebdbb3e4341395912d6ccc950b43e886781b`). And, if there is 31-bytes integer formatted by ASN.1, it will be padded into 32-length bytes with `0` byte. (e.g. `74e3e4d71e7385ae71042b0f99f7fbbf66e7760dd513ed2fcea754e2a9131c` → `0074e3e4d71e7385ae71042b0f99f7fbbf66e7760dd513ed2fcea754e2a9131c`).
  */
 export function parseSignature(buf: Buffer): { r: Buffer; s: Buffer } {
   const { result } = asn1js.fromBER(toArrayBuffer(buf));
