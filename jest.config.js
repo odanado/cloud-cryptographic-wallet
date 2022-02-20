3fb9748bbd1b08a19c9ccf66da88b2ce0e0b846f
odan <odan3240@gmail.com>
2022-02-21 00:48:13 +0900

    chore(jest): use ESM package

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 
 const config = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-ts",
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "html"],
   testEnvironment: "node",
+  transformIgnorePatterns: ["/node_modules/(?!(node-cjs-interop))"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
 };
 
 module.exports = config;
