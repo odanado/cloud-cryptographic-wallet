1726ee5dfcab16ef420de7cb90ec77fa850b82be
odan <odan3240@gmail.com>
2022-03-27 14:00:22 +0900

    feat(web3-provider-adapter): add getAccounts

---
+++ packages/web3-provider-adapter/src/get-accounts.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "vitest";
+
+import { getAccounts } from "./get-accounts.js";
+import { SignerForTest } from "../test-utils/signer-for-test.js";
+
+test("get addresses", async () => {
+  const singers = [new SignerForTest(), new SignerForTest()];
+
+  const addresses = await getAccounts(singers);
+
+  expect(addresses[0]).toBe(
+    (await singers[0].getPublicKey()).toAddress().toString()
+  );
+  expect(addresses[1]).toBe(
+    (await singers[1].getPublicKey()).toAddress().toString()
+  );
+});
