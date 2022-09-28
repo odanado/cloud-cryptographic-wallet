1166141e4f42b903e36ce21435d7651500eecea3
odan <odan3240@gmail.com>
2022-09-28 12:49:20 +0900

    test:  add test of signTypedData using ethers

--- test/e2e/legacy/web3-sign.spec.ts
+++ test/e2e/legacy/web3-sign.spec.ts
@@ -1,5 +1,10 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import Web3 from "web3";
+import { ethers } from "ethers";
+import {
+  recoverTypedSignature,
+  SignTypedDataVersion,
+} from "@metamask/eth-sig-util";
 
 import { KmsProvider } from "../../../aws-kms-packages/aws-kms-provider";
 import { getConfig } from "../../config";
@@ -21,4 +26,70 @@ it("web3.js", async () => {
     accounts[0].toLowerCase()
   );
   expect(web3.eth.accounts.recover(message, signature)).toBe(accounts[0]);
+});
+
+describe("ethers.js", () => {
+  it("signTypedData", async () => {
+    const domain = {
+      name: "Ether Mail",
+      version: "1",
+      chainId: 1,
+      verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+    };
+
+    // The named list of all type definitions
+    const types = {
+      Person: [
+        { name: "name", type: "string" },
+        { name: "wallet", type: "address" },
+      ],
+      Mail: [
+        { name: "from", type: "Person" },
+        { name: "to", type: "Person" },
+        { name: "contents", type: "string" },
+      ],
+    };
+
+    // The data to sign
+    const value = {
+      from: {
+        name: "Cow",
+        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+      },
+      to: {
+        name: "Bob",
+        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+      },
+      contents: "Hello, Bob!",
+    };
+
+    const baseProvider = new KmsProvider(rpcUrl, { region, keyIds: [keyId] });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const provider = new ethers.providers.Web3Provider(baseProvider as any);
+    const signer = provider.getSigner();
+    const signature = await signer._signTypedData(domain, types, value);
+
+    const address = await signer.getAddress();
+
+    const recovered = recoverTypedSignature({
+      data: {
+        domain,
+        types: {
+          EIP712Domain: [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "chainId", type: "uint256" },
+            { name: "verifyingContract", type: "address" },
+          ],
+          ...types,
+        },
+        primaryType: "Mail",
+        message: value,
+      },
+      signature,
+      version: SignTypedDataVersion.V4,
+    });
+
+    expect(recovered).toBe(address);
+  });
 });
